82361ceabddbfd14ab741af2d6f61a695e1768a2
RonobirDas <mookherj@unlv.nevada.edu>
2016-03-14 19:28:36 -0700

    Initial Single Cycle Changes upload

--- Baseline/alu.v
+++ Baseline/alu.v
@@ -1,9 +1,28 @@
-module alu (a,b,sel, out, zero);
-    input [31:0] a,b;
-    input [2:0] sel; 
-    output reg [31:0] out;
- output reg zero;
-  
+//-------------------------------------------------------------------
+//                                                                 
+//  COPYRIGHT (C) 2016, University of Nevada, Las Vegas
+//                                                                  
+//  THIS FILE MAY NOT BE MODIFIED OR REDISTRIBUTED WITHOUT THE      
+//  EXPRESSED WRITTEN CONSENT OF RONOBIR DAS, JONATHAN YOUNG, EMMA REGENTOVA
+//                                                                  
+//  Name:                   University of Nevada, Las Vegas         
+//  university_address1          4505 S. Maryland Parkway, Las Vegas, NV, 89154               
+//  unlv_address2         ece.unlv.edu                                     
+//-------------------------------------------------------------------
+// Title       : dmem.v
+// Author      : RONOBIR DAS & JONATHAN YOUNG
+// Description : Data Memory for the MIPS Processor. Modification of H&H text code to include byte handling
+//               
+// $Id$ 
+//-------------------------------------------------------------------
+`timescale 1ns / 1ps 
+module alu (
+    input [31:0] a,b,
+    input [3:0] sel,
+    input [4:0] shamt,
+    output reg [31:0] out,
+ output reg zero
+  );
   initial
   begin
   out = 0;
@@ -12,7 +31,12 @@ module alu (a,b,sel, out, zero);
     always @ (*) 
     begin 
         case(sel) 
-            3'b000: 
+default:
+begin
+out = 0; 
+zero = 1'b0; 
+end
+            4'b0000: //AND 0
 	begin
 		out=a & b; 
 		if (out == 0)
@@ -20,7 +44,7 @@ module alu (a,b,sel, out, zero);
 		else
 		zero = 0;
           end                   
-            3'b001:
+            4'b0001: // OR 1
 	begin
 		out= a | b; 
 		if (out == 0)
@@ -28,7 +52,7 @@ module alu (a,b,sel, out, zero);
 		else
 		zero = 0; 
 end		
-            3'b110: 
+            4'b0110: // SUB 6
 	begin
 		out=a-b;  
 		if (out == 0)
@@ -36,7 +60,7 @@ end
 		else
 		zero = 0;
           end              
-            3'b010: 
+            4'b0010: // ADD 2
 	begin
 		out=a+b;  
 		if (out == 0)
@@ -44,12 +68,44 @@ end
 		else
 		zero = 0;
           end         
-            3'b111: 
+            4'b0111: // SLT 7
            begin
 		 if ( a < b)
 		 out = 1;  
 		else
 		out=0;
+          end 
+ 4'b0011:  // SLL 3
+           begin
+out = b << shamt;
+if (out == 0)
+		 zero = 1;  
+		else
+		zero = 0; 
+          end 
+ 4'b0101: // SRL 5
+begin
+out = b >> shamt;
+		if (out == 0)
+		 zero = 1;  
+		else
+		zero = 0;
+end
+ 4'b0100:  // NOR 4
+           begin
+		out= ~(a | b); 
+		if (out == 0)
+		 zero = 1;  
+		else
+		zero = 0;
+          end 
+4'b1000: // XOR 8 
+           begin
+		out= a ^ b; 
+		if (out == 0)
+		 zero = 1;  
+		else
+		zero = 0;
           end 
         endcase
     end
--- Baseline/datapth.v
+++ Baseline/datapth.v
@@ -1,26 +1,49 @@
+//-------------------------------------------------------------------
+//                                                                 
+//  COPYRIGHT (C) 2016, University of Nevada, Las Vegas
+//                                                                  
+//  THIS FILE MAY NOT BE MODIFIED OR REDISTRIBUTED WITHOUT THE      
+//  EXPRESSED WRITTEN CONSENT OF RONOBIR DAS, JONATHAN YOUNG, EMMA REGENTOVA
+//                                                                  
+//  Name:                   University of Nevada, Las Vegas         
+//  university_address1          4505 S. Maryland Parkway, Las Vegas, NV, 89154               
+//  unlv_address2         ece.unlv.edu                                     
+//-------------------------------------------------------------------
+// Title       : dmem.v
+// Author      : RONOBIR DAS & JONATHAN YOUNG
+// Description : Data Memory for the MIPS Processor. Modification of H&H text code to include byte handling
+//               
+// $Id$ 
+//------------------------------------------------------------------- 
+`timescale 1ns / 1ps
+//Inputs and Outputs
 module datapath (input clk, reset,
 input memtoreg, pcsrc,
 input alusrc, regdst,
-input regwrite, jump,
-input [2:0] alucontrol,
+input regwrite, 
+input [4:0] jump, //jump signal now controls 4 muxes
+input [3:0] alucontrol, //modified, used to be 3 bits, now 4
 output zero,
 output [31:0] pc,
 input [31:0] instr,
 output [31:0] aluout, writedata,
 input [31:0] readdata);
-
+//Wires and Regs
 wire [4:0] writereg;
 wire [31:0] pcnext, pcnextbr, pcplus4, pcbranch;
 wire [31:0] signimm, signimmsh;
 wire [31:0] srca, srcb;
 wire [31:0] result;
+
+//Design Instantiations
 // next PC logic
 flopr #(32) pcreg(clk, reset, pcnext, pc);
 adder pcadd1 (pc, 32'b100, pcplus4);
 sl2 immsh(signimm, signimmsh); 
 adder pcadd2(pcplus4, signimmsh, pcbranch);
 mux2 #(32) pcbrmux(pcplus4, pcbranch, pcsrc, pcnextbr);
-mux2 #(32) pcmux(pcnextbr, {pcplus4[31:28], instr[25:0], 2'b00},jump, pcnext);
+mux2 #(32) pcmux(pcnextbr, {pcplus4[31:28], instr[25:0], 2'b00},jump[0], pcnext);
+
 // register file logic
 regfile rf(clk, regwrite, instr[25:21],
 instr[20:16], writereg, result, srca, writedata);
@@ -29,5 +52,5 @@ mux2 #(32) resmux(aluout, readdata, memtoreg, result);
 signext se(instr[15:0], signimm);
 // ALU logic
 mux2 #(32) srcbmux(writedata, signimm, alusrc, srcb);
-alu alu(srca, srcb, alucontrol, aluout, zero);
+alu alu(srca, srcb, alucontrol,instr[10:6], aluout, zero); // modified, added in the shamt line
 endmodule
